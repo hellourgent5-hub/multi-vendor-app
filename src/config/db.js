403a2d4f6c989bff0604acac777439e9cae44a75
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-17 21:41:11 +0530

    Add files via upload

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB connection failed", error);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
     process.exit(1);
   }
 };
