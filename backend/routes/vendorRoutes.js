e47d0bdd1f437355cfb590f84a8a66c5177fdcd2
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-17 16:55:58 +0530

    Update vendorRoutes.js

--- backend/routes/vendorRoutes.js
+++ backend/routes/vendorRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { registerVendor, loginVendor } = require('../controllers/vendorController');
+
+router.post('/register', registerVendor);
+router.post('/login', loginVendor);
+
+module.exports = router;
 
