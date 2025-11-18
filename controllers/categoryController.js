9934cd3ed57ad82c572f96a252220f610b73ee2e
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-18 23:35:53 +0530

    Create categoryController.js

---
+++ controllers/categoryController.js
@@ -0,0 +1,8 @@
+export const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching categories" });
+  }
+};
