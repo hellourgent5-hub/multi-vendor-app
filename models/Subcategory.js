0727e9630c58d622cbaf7446e470ebcb5a1cfbcd
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-19 22:03:39 +0530

    Update Subcategory.js

--- models/Subcategory.js
+++ models/Subcategory.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
-const subCategorySchema = new mongoose.Schema({
-  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
-  name: { type: String, required: true }
+const subcategorySchema = new mongoose.Schema({
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
-export default mongoose.model("Subcategory", subCategorySchema);
+export default mongoose.model("Subcategory", subcategorySchema);
+p
