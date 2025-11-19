63a0d45e2d477920dd1d59233edd1f0752906b5e
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-19 22:04:11 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-  module: { type: mongoose.Schema.Types.ObjectId, ref: "Module", required: true },
-  name: { type: String, required: true }
+  module: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Module", 
+    required: true 
+  },
+  name: { 
+    type: String, 
+    required: true 
+  }
 }, { timestamps: true });
 
 export default mongoose.model("Category", categorySchema);
