b9a62f557a99b2122c2bd09b4087eb33df575fc1
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-19 21:16:28 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -2,13 +2,26 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
     vendor: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true },
+
     name: { type: String, required: true },
     description: String,
     price: { type: Number, required: true },
     stock: { type: Number, default: 0 },
+
+    // Add images array
     images: [String],
-    category: String,
-    reviews: [{ user: {type: mongoose.Schema.Types.ObjectId, ref:'User'}, rating: Number, comment: String }]
+
+    // Add these two new fields
+    category: { type: String, required: true },
+    subcategory: { type: String },
+
+    reviews: [
+        {
+            user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+            rating: Number,
+            comment: String
+        }
+    ]
 }, { timestamps: true });
 
 export default mongoose.model('Product', productSchema);
