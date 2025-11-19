d9115c714879bd92f221c3729a4ba720002d459e
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-19 22:11:17 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import { createProduct, getProducts, getProductById, deleteProduct } from '../controllers/productController.js';
-import { protect, vendor, admin } from '../middlewares/authMiddleware.js';
+import express from "express";
+import {
+  createProduct,
+  getProducts,
+  getProduct,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// Get all products
-router.get('/', getProducts);
+// CREATE PRODUCT
+router.post("/", createProduct);
 
-// Get product by id
-router.get('/:id', getProductById);
+// GET ALL PRODUCTS
+router.get("/", getProducts);
 
-// Create product (vendor only)
-router.post('/', protect, vendor, createProduct);
-
-// Delete product (vendor or admin)
-router.delete('/:id', protect, deleteProduct);
+// GET ONE PRODUCT
+router.get("/:id", getProduct);
 
 export default router;
