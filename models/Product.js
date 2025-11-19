c34fcf739c7b02228c9e028a2102eaa3df57cb5e
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-19 22:06:34 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,22 +1,44 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    vendor: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true },
-    module: { type: mongoose.Schema.Types.ObjectId, ref: "Module", required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
-    subcategory: { type: mongoose.Schema.Types.ObjectId, ref: "Subcategory", required: true },
+  vendor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Vendor",
+    required: true
+  },
 
-    name: { type: String, required: true },
-    description: String,
-    price: { type: Number, required: true },
-    stock: { type: Number, default: 0 },
-    images: [String],
+  module: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Module",
+    required: true
+  },
 
-    reviews: [{
-        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        rating: Number,
-        comment: String
-    }]
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true
+  },
+
+  subcategory: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subcategory",
+    required: true
+  },
+
+  name: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+  stock: { type: Number, default: 0 },
+
+  images: [String],
+
+  reviews: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      rating: Number,
+      comment: String
+    }
+  ]
 }, { timestamps: true });
 
-export default mongoose.model('Product', productSchema);
+export default mongoose.model("Product", productSchema);
