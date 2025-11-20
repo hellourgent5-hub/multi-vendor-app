51b7f1dfc84d8fd58eb65db6852cbbb8b501cb1f
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-20 10:29:49 +0530

    Update and rename category.js to Category.js

--- models/Category.js
+++ models/Category.js
@@ -12,4 +12,6 @@ const categorySchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-export default mongoose.model("Category", categorySchema);
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
