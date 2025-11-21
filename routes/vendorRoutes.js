9988d8bd6831e91a9bf691b0330db7cf716c7f53
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-21 09:36:06 +0530

    Update vendorRoutes.js

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,18 +1,20 @@
 import express from "express";
-import {
-  getVendors,
-  getVendor,
-  createVendor,
-  updateVendor,
-  deleteVendor
-} from "../controllers/vendorController.js";
+import { 
+    getVendors,
+    getVendor,
+    createVendor,
+    updateVendor,
+    deleteVendor, // Line 7 - This must be exported by the controller
+    approveVendor 
+} from '../controllers/vendorController.js'; 
 
 const router = express.Router();
 
+router.post("/", createVendor);
 router.get("/", getVendors);
 router.get("/:id", getVendor);
-router.post("/", createVendor);
 router.put("/:id", updateVendor);
 router.delete("/:id", deleteVendor);
+router.put("/approve/:id", approveVendor); // Assuming you use this route
 
 export default router;
